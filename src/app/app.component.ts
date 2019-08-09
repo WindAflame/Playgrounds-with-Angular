8780b074beaa2c972166b40e63fccae930066bb1
WindAflame <dev.windaflame@gmail.com>
2019-08-09 11:52:01 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  exampleForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.exampleForm = this.fb.group({
+      text1: [''],
+      text2: [''],
+      text3: new FormControl({value: '', disabled: true}),
+    });
+
+   this.exampleForm.get('text1').disable();
+   this.exampleForm.get('text3').enable();
+  }
+
+  doNothing() {}
+
+}
