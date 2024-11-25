456871093c56d8e471db14d2000387a9ecddac37
WindAflame <dev.windaflame@gmail.com>
2024-11-25 08:57:55 +0000

    Add provider to extend title

--- TitleStrategy/src/app/app-routing.module.ts
+++ TitleStrategy/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HomeComponent } from './home/home.component';
 
@@ -7,17 +7,31 @@ const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent,
-    title: "Welcome - TitleStrategy",
+    // title: "Welcome",
   },
   {
     path: 'home',
     component: HomeComponent,
-    title: "Home - TitleStrategy",
+    title: "Home",
   }
 ];
 
+// Advanced Title Strategy
+@Injectable()
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      document.title = `${title} - TitleStrategy`;
+    } else {
+      document.title = `Welcome`;
+    }
+  }
+}
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: TitleStrategy,  useClass: TemplatePageTitleStrategy}]
 })
 export class AppRoutingModule { }
