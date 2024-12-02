ca34c75c4fca49e94d497c7a764372f112403dae
WindAflame <dev.windaflame@gmail.com>
2024-12-02 15:58:33 +0100

    feat: add two counter with different change detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-default-strategy></app-default-strategy>
+    <app-on-push-strategy></app-on-push-strategy>
+  `
 })
 export class AppComponent {
   title = 'MyApp';
